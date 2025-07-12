548ec5185d77bc1c1aecf00aa987072416e34a1e
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-07-12 22:12:08 +0200

    agragamos manejo de errores

--- src/Components/FetchComponent.jsx
+++ src/Components/FetchComponent.jsx
@@ -3,14 +3,22 @@ import { useEffect, useState } from 'react';
 export const FetchComponent = () => {
 
   const [users, setUsers] = useState();
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
+  const [errors, setErrors] = useState(false)
 
   const getFetch = async () => {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/users`)
-    const data = await response.json()
-    console.log(data)
-    setUsers(data)
-    setIsLoading(false)
+    try {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/users`)
+      const data = await response.json()
+      console.log(data)
+      setUsers(data)
+      setIsLoading(false)
+
+    } catch (error) {
+      setIsLoading(false)
+      setErrors(true)
+    }
+
   }
 
   useEffect(() => {
@@ -25,27 +33,31 @@ export const FetchComponent = () => {
         ? <div className="spinner-grow text-info" role="status">
           <span className="visually-hidden">Loading...</span>
         </div>
-        : <table className="table table-info">
-          <thead>
-            <tr>
-              <th scope="col">ID</th>
-              <th scope="col">Name</th>
-              <th scope="col">Email</th>
-              <th scope="col">Website</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map(user =>
-              <tr key={user.id}>
-                <th scope="row">{user.id}</th>
-                <td>{user.name}</td>
-                <td>{user.email}</td>
-                <td>{user.website}</td>
+        : errors
+          ? <div className="alert alert-danger text-center my-4" role="alert">
+            Ocurrió un error al cargar los datos. Intenta nuevamente más tarde.
+          </div>
+          : <table className="table table-info">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Website</th>
               </tr>
-            )}
+            </thead>
+            <tbody>
+              {users.map(user =>
+                <tr key={user.id}>
+                  <th scope="row">{user.id}</th>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.website}</td>
+                </tr>
+              )}
 
-          </tbody>
-        </table>
+            </tbody>
+          </table>
       }
 
     </>
